f81c0845577bb2f13df1f8b45f10c6ce41a8da68
deltaflyer17 <deltaaviation17@gmail.com>
2025-02-16 21:49:14 -0600

    CaseSens
    CaseSens

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import './styles/App.css';  // Keep your main styles here
 import Home from './components/Home';  
-import About from './components/about';
+import About from './components/About';
 import Projects from './components/Projects';
 import Contact from './components/Contact';
 import Navbar from './components/Navbar';
