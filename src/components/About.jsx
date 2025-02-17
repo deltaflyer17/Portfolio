32821ebf867fc8b202a70c711cdaa932073e1c67
deltaflyer17 <deltaaviation17@gmail.com>
2025-02-16 23:18:16 -0600

    Final Update

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -8,13 +8,13 @@ const About = () => {
                 <p>
                 My name is Ulises Gaytan, and I am a senior in high school. I plan to major in computer science during college.
                 I have been interested in software development for all of my life, and I am currently working on my web development skills as well as data sctructures and algorithms in Python.
-                Outside of programming I enjoy physical activity, and I have ran a marathon and I am now training for an Ironman 70.3
+                Outside of programming I enjoy physical activity, and I have ran a marathon and am now training for an Ironman 70.3 in May.
                 </p>
 
                 <p>
                 I am currently exploring all facets of computer science to eventually specialize in what I enjoy the most. 
-                Outside of web development I am interested in using Python for machine learning related to finance.
-                A future goal is to earn a master's degree in quantitative finance and eventually work as a quantitative trader.
+                Outside of web development I am interested in using Python, specifically for applications of AI/Machine Learning for finance.
+                A future goal is to earn a master's degree in quantitative finance, which combines computer science skills and finance/mathematics skills to eventually work as a quantitative trader.
                 </p>
        
                 <p>
